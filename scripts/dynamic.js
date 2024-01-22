edf556b62d374a53174adbe04f9c4c64d941c83f
bdgarat <garat.braian@gmail.com>
2024-01-22 11:14:44 -0300

    cambios en interceptos

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -4,9 +4,30 @@ bgD=()=>{   document.getElementById("cover").style.backgroundColor="#0a0a0a"
 document.getElementById("cover").style.color="white"
 }
 changeUsername=()=> {
+    //Obtengo el contenido del campo de texto
     var username = document.getElementById("userNameField").value;
-    if(username != "" || username != null) {
+   
+    // Si la lista no es vacia
+    if(username !== "" && username !== null) {
+        //Cambio el color del cover a verde
         document.getElementById("cover").style.backgroundColor = "green"
+
+        //Agrego el contenido del campo al texto de bienvenida
         document.getElementById("userNameText").textContent = username;
+
+        // Crear un nuevo elemento li
+        var nuevoElemento = document.createElement("li");
+
+        // Establecer el contenido del nuevo elemento con el valor ingresado
+        nuevoElemento.textContent = username;
+
+        // Obtener la lista ul existente
+        var miLista = document.getElementById("miLista");
+
+        // Agregar el nuevo elemento a la lista
+        miLista.appendChild(nuevoElemento);
+
+        // Limpiar el campo de texto después de agregar a la lista
+        document.getElementById("userNameField").value = "";
     }
 }
